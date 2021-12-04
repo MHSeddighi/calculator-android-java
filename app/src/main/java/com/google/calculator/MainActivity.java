915ce1385d2d3210799399ae11807e5a01160280
M.H.Seddighi <m.h.seddighi991@gamil.com>
2021-12-05 00:21:37 +0330

    fix bugs and change button type

--- app/src/main/java/com/google/calculator/MainActivity.java
+++ app/src/main/java/com/google/calculator/MainActivity.java
@@ -2,7 +2,9 @@ package com.google.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Second Activity").setOnMenuItemClickListener(item->{
+            startActivity(new Intent(MainActivity.this,SecondActivity.class));
+            return true;
+        });
+        return true;
     }
 }
