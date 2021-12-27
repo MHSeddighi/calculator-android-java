bacac0e5618d14dcd46081b52cdaa498e3ee7255
M.H.Seddighi <m.h.seddighi991@gamil.com>
2021-12-27 15:01:59 +0330

    update delete method in edit text

--- app/src/main/java/com/google/calculator/Monitor.java
+++ app/src/main/java/com/google/calculator/Monitor.java
@@ -29,22 +29,25 @@ public class Monitor {
             editText.setShowSoftInputOnFocus(false);
         }
         textView=activity.findViewById(R.id.textView);
+        editText.setSelection(0);
 
     }
 
     public void delete(boolean deleteAll,boolean fastDelete){
-        int length=editText.getText().length();
-        int selectionStart=editText.getSelectionStart();
+
+        int length         = editText.getText().length();
+        int selectionStart = editText.getSelectionStart();
+        int selectionEnd   = editText.getSelectionEnd();
+
         Editable text =editText.getText();
-        if(length!=0) {
+
+        if(length > 0) {
             if (deleteAll) {
                 text.clear();
             } else if(fastDelete){
                 fastDelete();
-            }else if(selectionStart == -1){
-                text.delete(length-1, length);
-            }else{
-                text.delete(selectionStart-1, selectionStart);
+            }else if(selectionStart > 1){
+                text.delete(selectionStart-1, selectionEnd);
             }
         }
     }
@@ -57,8 +60,8 @@ public class Monitor {
                 activity.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if(text.length()>1){
-                            text.delete(editText.getSelectionStart(), editText.getSelectionStart()+1);
+                        if(text.length()>1 && editText.getSelectionStart()>1 ){
+                            text.delete(editText.getSelectionStart()-2, editText.getSelectionEnd());
                         }
                     }
                 });
@@ -67,24 +70,30 @@ public class Monitor {
                     this.cancel();
                 }
             }
-        }, 150, 150);
+        }, 50, 80);
     }
 
     public Editable append(View view,Button previousClicked){
         Editable content =editText.getText();
         int length=content.length();
+
+        int selectionStart=editText.getSelectionStart();
+        int selectionEnd = editText.getSelectionEnd();
+
         if(view==null && previousClicked==null){
             return content;
         }
+
         if(!view.getTag().equals("operator")){
-            content.append(((Button)view).getText());
+            content.append(((Button)view).getText(),selectionStart,selectionEnd);
 
         }else if(length > 0 && previousClicked.getTag().equals("operator")){
             content.replace(length-1,length,((Button)view).getText());
 
         }else{
-            content.append(((Button)view).getText());
+            content.append(((Button)view).getText(),selectionStart,selectionEnd);
         }
+
         return content;
     }
 
